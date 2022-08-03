51a17e7c24e3269840b91d3cec4559c65198a29e
Iskander Siranov <iskander.siranov@gmail.com>
2022-08-03 18:49:01 +0600

    player controller's gravity settings change

--- Player/Player.gd
+++ Player/Player.gd
@@ -3,8 +3,8 @@ extends KinematicBody
 export var speed : float = 20
 export var acceleration : float = 15
 export var air_acceleration : float = 5
-export var gravity : float = 0.98
-export var max_terminal_velocity : float = 54
+export var gravity : float = 1.0
+export var max_terminal_velocity : float = 50
 export var jump_power : float = 20
 
 export(float, 0.1, 1) var mouse_sensitivity : float = 0.3
